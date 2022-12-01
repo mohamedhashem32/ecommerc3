94c727ece85952d008aca13878ab427d926a715a
Mohamed Hashem <76955799+mohamedhashem32@users.noreply.github.com>
2022-12-01 21:36:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const buttonn = document.getElementById("buttonn");
+const biggercontainer = document.querySelector(".bigger-container");
+
+buttonn.addEventListener("click",  
+   () => {
+
+    biggercontainer.style.display = "block"
+    ep.preventDefault()
+
+   }
+   )
+
+   signin.addEventListener("click",  
+   () => {
+
+    biggercontainer.style.display = "block"
+    ep.preventDefault()
+
+   }
+   )
